b2e407706d59b3d17947703e0d18bc10e1b3791e
Abi Turner <turner.abi@gmail.com>
2023-07-21 20:20:11 +0100

    file set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/jamming/i);
+  expect(titleElement).toBeInTheDocument();
 });
